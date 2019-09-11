42db3cad9dfdf833d689cb6a7d9baa24665b176e
Vannida Lim <vannida731@gmail.com>
2019-09-11 09:29:09 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,8 +12,8 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{name: "Steven", age: "30", status: "confused"}, {name: "Nurse", age: "44", status:"worried"}
-        #your hashes here!
+      :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
