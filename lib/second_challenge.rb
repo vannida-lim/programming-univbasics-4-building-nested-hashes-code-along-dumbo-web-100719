e82d7ffa9b5d7bffebf3233453636deca7e4231f
Vannida Lim <vannida731@gmail.com>
2019-09-11 09:15:54 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,16 +2,16 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch =>
-      :matriarch =>
-      :hero => 
+      :patriarch =>,
+      :matriarch =>,
+      :hero =>, 
       :hero_friends =>
       }, 
     :capulet => {
       #your key/value pairs here!
-      :patriarch =>
-      :matriarch =>
-      :hero => 
+      :patriarch =>,
+      :matriarch =>,
+      :hero =>, 
       :hero_friends =>
       }}
 
